a71a6dc7de99624f19107168029141932a8b048a
github username <github email address>
2019-11-22 11:40:40 -0800

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -14,20 +14,21 @@ const MenuBar = (props) => {
   */
 
   return (
+    
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick = {props.thisDerp} className="item active" id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={props.thisDerp} className="item" id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={props.thisDerp} className="item" id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={props.thisDerp} className="item" id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,20 +4,47 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      display: this.myRender.profile
+    }
+
+  }
+
+  myRender = {
+    profile: <Profile />,
+    photo: <Photos />,
+    cocktail: <Cocktails />,
+    pokemon: <Pokemon />
+
+  }
+
 
   render() {
 
     /*
 
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+    Replace the code below! Depending on what menu item is selected in the menu, I 
+    should render either a Profile, Photos, Cocktails, or Pokemon component.Think 
+    of a way to track which menu item was selected. Which component should have 
+    state? Which component should have methods to control state? Where should 
+    these methods be called?
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    
+
+    const clickFunction = (e) => {
+      let result = e.target.id
+      this.setState({display: this.myRender[result]})
+    }
+
+  const detailsToDisplay = <div>{this.state.display}</div>
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar thisDerp = {clickFunction}/>
         {detailsToDisplay}
       </div>
     )
